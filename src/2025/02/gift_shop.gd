73e5f4296a0c0322370dee0bd7842ac3c02d4e2c
Fernando Vilas <fvilas@iname.com>
2025-12-06 10:57:05 -0600

    Day 2 part 2

--- src/2025/02/gift_shop.gd
+++ src/2025/02/gift_shop.gd
@@ -30,10 +30,46 @@ func calc_part_one(data: Array) -> int:
 				bad_id_sum += to_test
 	return bad_id_sum
 
+func repeat_match(s: String, match_len: int) -> bool:
+	if s.length() % match_len != 0:
+		return false
+	var regex = RegEx.new()
+	# print("(\\d{%d})\\1{%d}" % [l-1, d[0].length() / l])
+	var reg_str = "(\\d{%d})\\1+" % [match_len]
+	# print(reg_str)
+	regex.compile(reg_str)
+	var result = regex.search(s)
+	if result && result.get_string() == s:
+		# print(result.get_string())
+		return true
+	return false
+
+func calc_part_two(data: Array) -> int:
+	var bad_id_sum: int = 0
+
+	for d in data:
+		var min_val: int = int(d[0])
+		var max_val: int = int(d[1])
+		for i in range(min_val, max_val+1):
+			var found: bool = false
+			for l:int in range(1, d[1].length()+1):
+				if d[0].length() % l == 0:
+					if repeat_match(str(i), l):
+						found = true
+
+				elif !found && d[1].length() > d[0].length() && d[1].length() % l == 0:
+					if repeat_match(str(i), l):
+						found = true
+
+			if found:
+				bad_id_sum += i
+			found = false
+	return bad_id_sum
+
 func run_part_one() -> int:
 	var data = input("input")
 	return calc_part_one(data)
 
 func run_part_two() -> int:
-	# var data = input("input")
-	return 0
+	var data = input("input")
+	return calc_part_two(data)
--- test/2025/02/gift_shop_test.gd
+++ test/2025/02/gift_shop_test.gd
@@ -22,3 +22,21 @@ func test_calc_part_one() -> void:
 	var data = runner.invoke("input", "example")
 	var result = runner.invoke("calc_part_one", data)
 	assert_that(result).is_equal(1227775554)
+
+func test_calc_part_two() -> void:
+	var runner = scene_runner("res://src/2025/02/GiftShop.tscn")
+	var data = runner.invoke("input", "example")
+	var result = runner.invoke("calc_part_two", data)
+	
+	assert_that(runner.invoke("calc_part_two", [["11","22"]])).is_equal(11+22)
+	assert_that(runner.invoke("calc_part_two", [["95","115"]])).is_equal(99+111)
+	assert_that(runner.invoke("calc_part_two", [["998","1012"]])).is_equal(999+1010)
+	assert_that(runner.invoke("calc_part_two", [["1188511880","1188511890"]])).is_equal(1188511885)
+	assert_that(runner.invoke("calc_part_two", [["222220","222224"]])).is_equal(222222)
+	assert_that(runner.invoke("calc_part_two", [["1698522","1698528"]])).is_equal(0)
+	assert_that(runner.invoke("calc_part_two", [["446443","446449"]])).is_equal(446446)
+	assert_that(runner.invoke("calc_part_two", [["38593856","38593862"]])).is_equal(38593859)
+	assert_that(runner.invoke("calc_part_two", [["565653","565659"]])).is_equal(565656)
+	assert_that(runner.invoke("calc_part_two", [["824824821","824824827"]])).is_equal(824824824)
+	assert_that(runner.invoke("calc_part_two", [["2121212118","2121212124"]])).is_equal(2121212121)
+	assert_that(result).is_equal(4174379265)
