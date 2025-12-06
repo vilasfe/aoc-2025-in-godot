8cc350620358b75697d9b4bcf55b54ebd86b48e1
Fernando Vilas <fvilas@iname.com>
2025-12-06 15:03:34 -0600

    Day 3 part 2

--- src/2025/03/lobby.gd
+++ src/2025/03/lobby.gd
@@ -21,7 +21,20 @@ func calc_part_one(data: PackedStringArray) -> int:
 	return joltage
 
 func calc_part_two(data: PackedStringArray) -> int:
-	return 0
+	var joltage: int = 0
+	for d: String in data:
+		var battery_array:Array = Array(d.split()).map(func(e: String) -> int: return e.to_int()).filter(func(e: int) -> bool: return e > 0)
+		var lights: Array[int] = []
+		var max_light_index: int = -1
+		for i: int in range(12, 0, -1):
+			var max_light = battery_array.slice(max_light_index+1,battery_array.size()-i+1).max()
+			max_light_index = battery_array.find(max_light, max_light_index+1)
+			lights.push_back(max_light)
+
+		var joltage_this_bank: int = lights.reduce	(func(accum: String, e: int) -> String: return accum + str(e), "").to_int()
+		joltage += joltage_this_bank
+		lights.clear()
+	return joltage
 
 func run_part_one() -> int:
 	var data = input("input")
--- test/2025/03/lobby_test.gd
+++ test/2025/03/lobby_test.gd
@@ -15,5 +15,11 @@ func test_calc_part_one() -> void:
 	assert_that(result).is_equal(357)
 
 func test_run_part_two() -> void:
-	# remove this line and complete your test
-	assert_not_yet_implemented()
+	var runner = scene_runner("res://src/2025/03/Lobby.tscn")
+	var data = runner.invoke("input", "example")
+	var result = runner.invoke("calc_part_two", data)
+	assert_that(runner.invoke("calc_part_two", [["987654321111111"]])).is_equal(987654321111)
+	assert_that(runner.invoke("calc_part_two", [["811111111111119"]])).is_equal(811111111119)
+	assert_that(runner.invoke("calc_part_two", [["234234234234278"]])).is_equal(434234234278)
+	assert_that(runner.invoke("calc_part_two", [["818181911112111"]])).is_equal(888911112111)
+	assert_that(result).is_equal(3121910778619)
