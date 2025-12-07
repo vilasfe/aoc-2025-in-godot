be830f29ce840178d2a3667b400f59ded46770ea
Fernando Vilas <fvilas@iname.com>
2025-12-07 09:54:03 -0600

    Day 5 part 2

--- src/2025/05/cafeteria.gd
+++ src/2025/05/cafeteria.gd
@@ -30,8 +30,25 @@ func calc_part_one(data: Dictionary) -> int:
 			good += 1
 	return good
 
+# this should be a lambda, but breaking out for debugging
+func accum_range(accum: int, e: Array) -> int:
+	return accum + e[1] - e[0] + 1
+
 func calc_part_two(data: Dictionary) -> int:
-	return 0
+	var sorted_ranges: Array[Array] = data.get("good_ranges")
+	sorted_ranges.sort()
+	var merged_ranges: Array[Array] = []
+	for r in sorted_ranges:
+		var merged_index: int = -1
+		for m in range(merged_ranges.size()):
+			if (r[0] >= merged_ranges[m][0] && r[0] <= merged_ranges[m][1]) || (r[1] >= merged_ranges[m][0] && r[1] <= merged_ranges[m][1]):
+				merged_index = m
+		if merged_index == -1:
+			merged_ranges.append(r)
+		else:
+			merged_ranges[merged_index] = [min(merged_ranges[merged_index][0], r[0]), max(merged_ranges[merged_index][1], r[1])]
+	var to_return: int = merged_ranges.reduce(accum_range, int(0))
+	return to_return
 
 func run_part_one() -> int:
 	var data = input("input")
